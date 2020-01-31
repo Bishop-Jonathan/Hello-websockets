c15f42e216bf5db36de56844b2fdff6c994008ff
Jonathan Bishop <Bishop.jonathane@gmail>
2020-01-30 18:54:49 -0600

    Set up the server to resolve issue#3

---
+++ app.js
@@ -0,0 +1,13 @@
+//npm install --save express http url ws.
+// server side js in node 
+
+const express = require('express'); // framework for server side script 
+const http = require('http');
+const app = express();
+
+app.use(express.static('public'));
+
+const server = http.createServer(app);
+server.listen(8080, ()=>{
+    console.log('app listening on port 8080');
+});
